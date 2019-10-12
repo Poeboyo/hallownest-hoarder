a092c8d8a33c64d88a457a75d317334d6830a78e
Jeffrey Poe <jeffreypoe@Jeffreys-MacBook-Pro-2.local>
2019-10-12 09:07:11 -0400

    https://github.com/Poeboyo/unit-4-game.git

---
+++ js.js
@@ -0,0 +1,406 @@
+
+
+
+
+
+
+
+//Create Display for Wins and Losses 
+//Create Random Score Generator
+//Create counter 
+    var wins = 0;
+    var losses = 0;
+    var randomNumber = Math.floor(19+ Math.random() * 101);
+    var playerScore = 0;
+    var randomCharm = Math.floor(1 + Math.random()* 44);
+
+    
+            
+            $("#randomNumberBox").text("Randomly Generated Number: " + randomNumber);
+            $("#winCount").text("Wins: " + wins);
+            $("#lossCount").text("Losses: " + losses);
+            $("#playScore").text("Your Score: " + playerScore);
+
+    //Buttons with Randomized Values
+    var butOne = Math.floor(1 + Math.random()*11);
+    var butTwo = Math.floor(1 + Math.random()*11);
+    var butThree = Math.floor(1 + Math.random()*11);
+    var butFour = Math.floor(1 + Math.random()*11);
+
+
+
+//Button 1
+    $('#button-1').on ('click', function(){
+    playerScore = playerScore + butOne;
+    
+    $('#playScore').text("Your Score: " + playerScore); 
+
+    if (playerScore === randomNumber ){
+          win();
+          reset();
+        }
+        else if (playerScore > randomNumber){
+          lose();
+          reset();
+          
+        }   
+    })
+
+//Button 2
+    $('#button-2').on ('click', function(){
+    playerScore = playerScore + butTwo;
+    
+    $('#playScore').text("Your Score: " + playerScore); 
+
+    if (playerScore == randomNumber ){
+          win();
+          reset();
+          
+        }
+        else if (playerScore > randomNumber){
+          lose();
+          reset();
+          
+        }   
+    })
+
+//Button 3
+    $('#button-3').on ('click', function(){
+    playerScore = playerScore + butThree;
+    
+    $('#playScore').text("Your Score: " + playerScore); 
+
+    if (playerScore == randomNumber ){
+          win();
+          reset();
+        }
+        else if (playerScore > randomNumber){
+          lose();
+          reset();
+        }   
+    })
+
+//Button 4
+    $('#button-4').on ('click', function(){
+    playerScore = playerScore + butFour;
+    
+    $('#playScore').text("Your Score: " + playerScore); 
+
+    if (playerScore == randomNumber ){
+          win();
+          reset();
+        }
+        else if (playerScore > randomNumber){
+          lose();
+          reset();
+        }   
+    })
+   
+   
+
+    
+//Create Reset Function
+    function reset(){
+     butOne = Math.floor(1 + Math.random()*11);
+     butTwo = Math.floor(1 + Math.random()*11);
+     butThree = Math.floor(1 + Math.random()*11);
+     butFour = Math.floor(1 + Math.random()*11);
+     playerScore = 0;
+    };
+    
+    function win(){
+       
+            wins++;
+            $("#winCount").text("Wins: "+wins);
+            reset();
+            
+        };
+    
+    function lose(){
+        
+            losses++;
+            $("#lossCount").text("Losses: "+losses);
+            reset()
+    }
+
+
+
+
+
+    //Charms per Justin's Request 
+var charms = [
+     {
+        name : "Wayward Compass",
+        desc : "Whispers its location to the bearer whenever a map is open.",
+        url : "https://vignette.wikia.nocookie.net/hollowknight/images/7/7d/Wayward_Compass.png/revision/latest?cb=20180909165457"
+    },
+
+    {
+      name : "Gathering Swarm",
+      desc : "A swarm will follow the bearer and gather up any loose Geo.",
+      url : "https://vignette.wikia.nocookie.net/hollowknight/images/8/8a/Gathering_Swarm.png/revision/latest?cb=20180909165419"
+    },
+
+    {
+      name : "Stalwart Shell",
+      desc : "When recovering from damage, the bearer will remain invulnerable for longer.",
+      url : "https://vignette.wikia.nocookie.net/hollowknight/images/f/f2/Stalwart_Shell.png/revision/latest?cb=20170330141533"
+    },
+
+    {
+      name : "Soul Catcher",
+      desc : "Increases the amount of SOUL gained when striking an enemy with the nail.",
+      url : "https://vignette.wikia.nocookie.net/hollowknight/images/c/ca/Soul_Catcher.png/revision/latest?cb=20180909165119"
+    },
+
+    {
+      name : "Shaman Stone",
+      desc : "Increases the power of spells, dealing more damage to foes.",
+      url : "https://vignette.wikia.nocookie.net/hollowknight/images/5/5e/Shaman_Stone.png/revision/latest?cb=20180909165308"
+    },
+
+    {
+      name : "Soul Eater",
+      desc : "Greatly increases the amount of SOUL gained when striking an enemy with the nail.",
+      url : "https://vignette.wikia.nocookie.net/hollowknight/images/6/6c/Soul_Eater.png/revision/latest?cb=20180909165007"
+    },
+
+    {
+      name : "Dashmaster",
+      desc : "The bearer will be able to dash more often as well as dash downwards.",
+      url : "https://vignette.wikia.nocookie.net/hollowknight/images/7/70/Dashmaster.png/revision/latest?cb=20180810194020"
+    },
+
+    {
+      name : "Thorns of Agony",
+      desc : "When taking damage, sprout thorny vines that damage nearby foes.",
+      url : "https://vignette.wikia.nocookie.net/hollowknight/images/8/8f/Thorns_of_Agony.png/revision/latest?cb=20180909165348"
+    },
+
+    {
+      name : "Fury of the Fallen",
+      desc : "When close to death, the bearer's strength will increase.",
+      url : "https://vignette.wikia.nocookie.net/hollowknight/images/4/4f/Fury_of_the_Fallen.png/revision/latest?cb=20180909171045"
+    },
+
+    {
+      name : "Fragile Heart",
+      desc : "Increases the health of the bearer. This charm is fragile, and will break if its bearer is killed.",
+      url : "https://vignette.wikia.nocookie.net/hollowknight/images/1/13/Fragile_Heart.png/revision/latest?cb=20180923025627"
+    },
+
+    {
+      name : "Fragile Greed",
+      desc : "Causes the bearer to find more Geo when defeating enemies. This charm is fragile, and will break if its bearer is killed.",
+      url : "https://vignette.wikia.nocookie.net/hollowknight/images/b/b6/Fragile_Greed.png/revision/latest?cb=20180923025659"
+    },
+
+    {
+      name : "Fragile Strength",
+      desc : "Strengthens the bearer, increasing the damage they deal to enemies with their nail. This charm is fragile, and will break if its bearer is killed.",
+      url : "https://vignette.wikia.nocookie.net/hollowknight/images/7/7b/Fragile_Strength.png/revision/latest?cb=20180923025436"
+    },
+
+    {
+      name : "Spell Twister",
+      desc : "Reduces the SOUL cost of casting spells.",
+      url : "https://vignette.wikia.nocookie.net/hollowknight/images/3/33/Spell_Twister.png/revision/latest?cb=20180909165049"
+    },
+
+    {
+      name : "Steady Body",
+      desc : "Keeps its bearer from recoiling backwards when they strike an enemy with a nail.",
+      url : "https://vignette.wikia.nocookie.net/hollowknight/images/f/f5/Steady_Body.png/revision/latest?cb=20180909165654"
+    },
+
+    {
+      name : "Heavy Blow",
+      desc : "Increases the force of the bearer's nail, causing enemies to recoil further when hit.",
+      url : "https://vignette.wikia.nocookie.net/hollowknight/images/f/f6/Heavy_Blow.png/revision/latest?cb=20180909165720"
+    },
+
+    {
+      name : "Quick Slash",
+      desc : "Allows the bearer to slash much more rapidly with their nail.",
+      url : "https://vignette.wikia.nocookie.net/hollowknight/images/5/5f/Quick_Slash.png/revision/latest?cb=20180909165747"
+    },
+
+    {
+      name : "Longnail",
+      desc : "Increases the range of the bearer's nail.",
+      url : "https://vignette.wikia.nocookie.net/hollowknight/images/d/d1/Longnail.png/revision/latest?cb=20180909165814"
+    },
+
+    {
+      name : "Mark of Pride",
+      desc : "Greatly increases the range of the bearer's nail.",
+      url : "https://vignette.wikia.nocookie.net/hollowknight/images/6/69/Mark_of_Pride.png/revision/latest?cb=20170508202722"
+    },
+
+    {
+      name : "Baldur Shell",
+      desc : "Protects its bearer with a hard shell while focusing SOUL.",
+      url : "https://vignette.wikia.nocookie.net/hollowknight/images/2/21/Baldur_Shell.png/revision/latest?cb=20180909165838"
+    },
+
+    {
+      name : "Flukenest",
+      desc : "Transforms the Vengeful Spirit spell into a horde of volatile baby flukes.",
+      url : "https://vignette.wikia.nocookie.net/hollowknight/images/7/79/Flukenest.png/revision/latest?cb=20180909165913"
+    },
+
+    {
+      name : "Defender's Crest",
+      desc : "Causes the bearer to emit a heroic odour.",
+      url : "https://vignette.wikia.nocookie.net/hollowknight/images/5/56/Defender%27s_Crest.png/revision/latest?cb=20180909165929"
+    },
+
+    {
+      name : "Glowing Womb",
+      desc : "Drains the SOUL of its bearer and uses it to birth hatchlings.",
+      url : "https://vignette.wikia.nocookie.net/hollowknight/images/c/c6/Glowing_Womb.png/revision/latest?cb=20180909165949"
+    },
+
+    {
+      name : "Quick Focus",
+      desc : "Increases the speed of focusing SOUL, allowing the bearer to heal faster.",
+      url : "https://vignette.wikia.nocookie.net/hollowknight/images/6/6a/Quick_Focus.png/revision/latest?cb=20180909170014"
+    },
+
+    {
+      name : "Deep Focus",
+      desc : "The bearer will focus SOUL at a slower rate, but the healing effect will double.",
+      url : "https://vignette.wikia.nocookie.net/hollowknight/images/e/ea/Deep_Focus.png/revision/latest?cb=20180909170038"
+    },
+
+    {
+      name : "Lifeblood Heart",
+      desc : "When resting, the bearer will gain a coating of lifeblood that protects from a modest amount of damage.",
+      url : "https://vignette.wikia.nocookie.net/hollowknight/images/7/7c/Lifeblood_Heart.png/revision/latest?cb=20180909170057"
+    },
+
+    {
+      name : "Lifeblood Core",
+      desc : "When resting, the bearer will gain a coating of lifeblood that protects from a large amount of damage.",
+      url : "https://vignette.wikia.nocookie.net/hollowknight/images/8/81/Lifeblood_Core.png/revision/latest?cb=20180909170116"
+    },
+
+    {
+      name : "Joni's Blessing",
+      desc : "The bearer will have a healthier shell and can take more damage, but they will not be able to heal themselves by focusing SOUL.",
+      url : "https://vignette.wikia.nocookie.net/hollowknight/images/6/67/Joni%27s_Blessing.png/revision/latest?cb=20180909170135"
+    },
+
+    {
+      name : "Grubsong",
+      desc : "Gain SOUL when taking damage.",
+      url : "https://vignette.wikia.nocookie.net/hollowknight/images/7/78/Grubsong.png/revision/latest?cb=20180909170155"
+    },
+
+    {
+      name : "Grubberfly's Elegy",
+      desc : "Imbues weapons with a holy strength. When the bearer is at full health, they will fire beams of white-hot energy from their nail.",
+      url : "https://vignette.wikia.nocookie.net/hollowknight/images/b/bd/Grubberfly%27s_Elegy.png/revision/latest?cb=20180909170214"
+    },
+
+    {
+      name : "Hiveblood",
+      desc : "Heals the bearer's wounds over time, allowing them to regain health without focusing SOUL.",
+      url : "https://vignette.wikia.nocookie.net/hollowknight/images/e/eb/Hiveblood.png/revision/latest?cb=20170508194807"
+    },
+
+    {
+      name : "Spore Shroom",
+      desc : "When focusing SOUL, emit a spore cloud that slowly damages enemies.",
+      url : "https://vignette.wikia.nocookie.net/hollowknight/images/7/78/Spore_Shroom.png/revision/latest?cb=20170508194757"
+    },
+
+    {
+      name : "Sharp Shadow",
+      desc : "When using Shadow Dash, the bearer's body will sharpen and damage enemies.",
+      url : "https://vignette.wikia.nocookie.net/hollowknight/images/1/13/Sharp_Shadow.png/revision/latest?cb=20170508194735"
+    },
+
+    {
+      name : "Shape of Unn",
+      desc : "While focusing SOUL, the bearer will take on a new shape and can move freely to avoid enemies.",
+      url : "https://vignette.wikia.nocookie.net/hollowknight/images/b/b4/Shape_of_Unn.png/revision/latest?cb=20180909170919"
+    },
+
+    {
+      name : "Nailmaster's Glory",
+      desc : "Increases the bearer's mastery of Nail Arts, allowing them to focus their power faster and unleash arts sooner.",
+      url : "https://vignette.wikia.nocookie.net/hollowknight/images/0/0f/Nailmaster%27s_Glory.png/revision/latest?cb=20170508194621"
+    },
+
+    {
+      name : "Dream Wielder",
+      desc : "Allows the bearer to charge the Dream Nail faster and collect more SOUL when striking foes.",
+      url : "https://vignette.wikia.nocookie.net/hollowknight/images/9/94/Dream_Wielder.png/revision/latest?cb=20180909170602"
+    },
+
+    {
+      name : "Kingsoul",
+      desc : "Holy charm symbolising a union between higher beings. The bearer will slowly absorb the limitless SOUL contained within. Opens the way to a birthplace.",
+      url : "https://vignette.wikia.nocookie.net/hollowknight/images/3/34/Kingsoul.png/revision/latest?cb=20180909170505"
+    },
+
+    {
+      name : "Void Heart",
+      desc : "Unifies the void under the bearer's will. This charm is a part of its bearer and can not be unequipped.",
+      url : "https://vignette.wikia.nocookie.net/hollowknight/images/b/bb/Void_Heart.png/revision/latest?cb=20180909170526"
+    },
+
+    {
+      name : "Unbreakable Heart",
+      desc : "Increases the health of the bearer.",
+      url : "https://vignette.wikia.nocookie.net/hollowknight/images/1/15/Unbreakable_Heart.png/revision/latest?cb=20171028004956"
+    },
+
+    {
+      name : "Unbreakable Greed",
+      desc : "Causes the bearer to find more Geo when defeating enemies.",
+      url : "https://vignette.wikia.nocookie.net/hollowknight/images/2/2a/Unbreakable_Greed.png/revision/latest?cb=20171028004801"
+    },
+
+    {
+      name : "Unbreakable Strength",
+      desc : "Strengthens the bearer, increasing the damage they deal to enemies with their nail.",
+      url : "https://vignette.wikia.nocookie.net/hollowknight/images/a/ac/Unbreakable_Strength.png/revision/latest?cb=20171028004739"
+    },
+
+    {
+      name : "Dreamshield",
+      desc : "Conjures a shield that follows the bearer and attempts to protect them.",
+      url : "https://vignette.wikia.nocookie.net/hollowknight/images/4/47/Dreamshield.png/revision/latest?cb=20180909171219"
+    },
+
+    {
+      name : "Weaversong",
+      desc : "Summons weaverlings to give the lonely bearer some companionship and protection.",
+      url : "https://vignette.wikia.nocookie.net/hollowknight/images/2/26/Weaversong.png/revision/latest?cb=20171028131632"
+    },
+
+    {
+      name : "Sprintmaster",
+      desc : "Increases the running speed of the bearer, allowing them to avoid danger or overtake rivals.",
+      url : "https://vignette.wikia.nocookie.net/hollowknight/images/e/e9/Sprintmaster.png/revision/latest?cb=20171028131625"
+    },
+
+    {
+      name : "Carefree Melody",
+      desc : "Contains a song of protection that may defend the bearer from damage.",
+      url : "https://vignette.wikia.nocookie.net/hollowknight/images/c/c4/Carefree_Melody.png/revision/latest?cb=20180909171307"
+    },
+
+    {
+      name : "Grimmchild",
+      desc : "Worn by those who take part in the Grimm Troupe's Ritual. The bearer must seek the Grimmkin and collect their flames. Uncollected flames will appear on the bearer's map.",
+      url : "https://vignette.wikia.nocookie.net/hollowknight/images/9/91/Grimmchild.png/revision/latest?cb=20171028131618"
+    },
+
+];
+    
+function showCharm(){
+
+};
